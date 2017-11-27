a64f30127f7caee072150061f0a688d14a37c4cf
andieh <andieh@localhost>
2017-11-27 20:09:37 +0100

    more new files

---
+++ raspi/serialTest.py
@@ -0,0 +1,63 @@
+import serial
+import time 
+from thread import start_new_thread
+
+class CarPC:
+    def __init__(self):
+        self.con = serial.Serial('/dev/serial0', 9600, timeout=1)  # open serial port
+        print self.con.name         # check which port was really used
+        self.doListen = False
+        self.disconnected = True
+
+    def listen(self):
+        self.doListen = True
+        self.disconnected = False
+        while self.doListen:
+            r = self.con.readline()
+            if not r:
+                continue
+            print r
+        self.disconnected = True
+
+    def stopListen(self):
+        self.doListen = False
+
+    def close(self):
+        self.stopListen()
+        while self.disconnected:
+            print "waiting for closed connection"
+            time.sleep(1)
+        self.con.close()
+
+    def send(self, what):
+        self.con.write(what)
+
+if __name__ == "__main__":
+    car = CarPC()
+    start_new_thread(car.listen, ())
+    
+    while 1:
+        nc = raw_input()
+        if nc:
+            car.send(nc)
+
+    car.close()
+
+    """ser = serial.Serial(
+    nc = "a"
+    while 1:
+        ser.write(nc)     # write a string
+        if nc:
+            print "sending ", nc, ord(nc), hex(ord(nc))
+        nc = ""
+        while True:
+            r = ser.readline()
+            for a in r:
+                o = ord(a)
+                c = chr(o)
+                print a, o, c
+            break
+        #nc = raw_input()
+        #print type(nc)
+    #print int(r)
+    ser.close()"""
